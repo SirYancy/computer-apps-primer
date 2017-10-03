f166ca8e2c752a2a34e87ed8ba62f537649379fc
Eric Kuha <erickuha@gmail.com>
2017-10-03 14:26:23 -0500

    weather2

--- ch4-spreadsheets/res/scripts/weather.py
+++ ch4-spreadsheets/res/scripts/weather.py
@@ -3,12 +3,8 @@ import json
 
 api_key = "59ecb95e623a2b6c"
 
-r = requests.get("http://api.wunderground.com/api/" + api_key + "forecast/q/France/Pars.json")
+r = requests.get("http://api.wunderground.com/api/59ecb95e623a2b6c/history_20160405/q/airport/kbji.json")
 
 data = r.json()
 
-for day in data['forecast']['simpleforecast']['forecastday']:
-    print(day['date']['weekday'] + ":")
-    print("Conditions: ", day['conditions'])
-    print("High: ", day['high']['celsius'] + "C", "Low: ", day['low']['celsius'] + "C")
-
+print(data)
